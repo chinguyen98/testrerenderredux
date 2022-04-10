1c7dab36aa87969ace9136109086fed00546b590
Nguyễn Đắc Chí <chind@hasaki.vn>
2022-04-10 21:42:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { increaseAction } from './reducer/app/app.action';
 import Test from './components/Test';
 import MiniTest from './components/MiniTest';
 import useConnectWsk from './useConnectWsk';
 import useTest from './hooks/useTest';
+import { increaseThreeAction } from './reducer/tada/tada.action';
 
 function App() {
   console.log('app rerender')
-  // const { count } = useSelector(state => state.app);
+  const count = useSelector(state => state.app.count);
   const { countTwo } = useSelector(state => state.app);
+  const app = useSelector(state => state.app);
+  const { increaseThree } = useSelector(state => state.tada.ecec.blabla);
+  const { blaqq } = useSelector(state => state.tada.hihi);
+  const test = useSelector(state => state.tada.ecec.blabla)
+  const soso = useSelector(state => state.tada.hihi.ola)
+  const blaqq1 = useSelector(state => state.tada.hihi.blaqq)
 
   const dispatch = useDispatch();
 
@@ -19,7 +26,8 @@ function App() {
   // useTest();
 
   const handleClick = () => {
-    dispatch(increaseAction())
+    // dispatch(increaseAction())
+    dispatch(increaseThreeAction())
   }
 
   return (
--- src/reducer/tada/tada.reducer.js
+++ src/reducer/tada/tada.reducer.js
@@ -2,19 +2,33 @@ import { TADA } from "./tada.action";
 
 const initialState = {
   ecec: {
-    increaseThree: 0,
+    blabla: {
+      increaseThree: 0,
+      sosad: { e: 5 },
+    }
+  },
+  hihi: {
+    ola: [{ a: 1, b: 2 }],
+    soso: 0,
+    blaqq: { c: 5, d: -1 }
   }
 }
 
 const tadaReducer = (state = initialState, action) => {
   switch (action.type) {
     case TADA.INCREASE_THREE: {
-      const oldCount = state.ecec.increaseThree
+      const oldCount = state.ecec.blabla.increaseThree;
+      const newState = JSON.parse(JSON.stringify(state));
+      newState.ecec.blabla.increaseThree = oldCount + 1;
+      return newState;
       return {
         ...state,
         ecec: {
           ...state.ecec,
-          increaseThree: oldCount + 1,
+          blabla: {
+            ...state.ecec.blabla,
+            increaseThree: oldCount + 1,
+          }
         }
       }
     }
